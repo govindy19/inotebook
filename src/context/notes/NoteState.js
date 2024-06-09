b96ea6a2ee7f0ae987890c4ca3648f813e8b9f8e
govindy19 <130302812+govindy19@users.noreply.github.com>
2024-06-09 10:40:49 +0530

    final commit

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-  const host = "http://localhost:8000"
+  const host = "https://inotebook-38hk.onrender.com"
   const notesInitial = []
   const [notes,setNotes]= useState(notesInitial)
    //Get all notes
